55b72c69e652154c47afeb67c54dc30a4cc233ca
sergio portela <sergio.portela@herramientasdemejora.com>
2022-05-12 00:00:44 -0500

    Se inicia AD

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'activedirectory';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = 'Sp0rt3l4#91';
+$CFG->prefix    = 'ad_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://activedirectory.test';
+$CFG->dataroot  = '/var/www/moodledataad';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
